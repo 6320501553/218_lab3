1796baefc453bc4e7af04cca30917f0f63912b9e
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-18 15:24:56 +0700

    edit

--- 6320501553_3_1.c
+++ 6320501553_3_1.c
@@ -1,30 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int a[5][4],i,j,s[5]= {0},max;
+    int a[5][4],i,j,s[5]= {0},max=0;
     for(i=0; i<5; i++)
     {
-        for(j=0; j<4; j++)
+        for(j=0;j<4;j++)
         {
             scanf("%d",&a[i][j]);
             s[i]+=a[i][j];
         }
-    }
-    max=s[0];
-    for(i=1; i<5; i++)
-    {
         if(max<s[i])
         {
             max=s[i];
         }
     }
-    for(i=1; i<5; i++)
+    for(i=0; i<5; i++)
     {
-        if(s[i]==max)
+        if(max == s[i])
         {
-            printf("%d %d",i+1,max);
             break;
         }
     }
+     printf("%d %d",i+1,max);
     return 0;
 }
