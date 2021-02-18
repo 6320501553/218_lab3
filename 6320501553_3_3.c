17e2e50c02fc567cfdc9e9979e1c91351595a9b6
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-18 16:20:49 +0700

    add

--- 6320501553_3_3.c
+++ 6320501553_3_3.c
@@ -3,9 +3,9 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=0; i<=n; i++)
+    for(i=0; i<n+1; i++)
     {
-        for(j=2; j<n; j++)
+        for(j=2; j<n+1; j++)
         {
             if(n%j==0)
             {
@@ -14,6 +14,8 @@ int main()
             }
         }
         n=n/j;
+        if(n==1)
+            printf("0\n");
     }
     return 0;
 }
