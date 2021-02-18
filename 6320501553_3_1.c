dc1840fafbf19bc3f5f6628e4c4f97c6e7b18dfd
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-18 15:15:28 +0700

    add

---
+++ 6320501553_3_1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a[5][4],i,j,s[5]= {0},max;
+    for(i=0; i<5; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            scanf("%d",&a[i][j]);
+            s[i]=s[i]+a[i][j];
+        }
+        printf("%d\n",s[i]);
+    }
+    max=s[0];
+    for(i=1; i<5; i++)
+    {
+        if(max<s[i])
+        {
+            max=s[i];
+        }
+    }
+    for(i=1; i<5; i++)
+    {
+        if(s[i]==max)
+        {
+            printf("%d %d",i+1,max);
+            break;
+        }
+    }
+    return 0;
+}
